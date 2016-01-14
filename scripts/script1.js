98e61e3110d6ab7188fbcca75657bd45c6c9b6f0
legetic <Legetic@hotmail.com>
2016-01-14 22:27:59 +0100

    sadas

--- scripts/script1.js
+++ scripts/script1.js
@@ -43,7 +43,7 @@ mobile = true;
     $body.addClass('notMobile');
   }
 
-  if(Modernizr.mq('(max-width: 992px)')){
+  if($(window).width() < 992){
   $body.addClass('small');
   }else{
     $body.removeClass('small');
@@ -190,7 +190,7 @@ if(!infoTabOpen && !mobile){
   $('#garanti').resizeToParentLess();
 
 $(window).resize(function() {
-  if(Modernizr.mq('(max-width: 992px)')){
+  if($(window).width() < 992){
 $body.addClass('small');
   }else{
     $body.removeClass('small');
