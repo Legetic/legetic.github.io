5493300916ed460664659ce62d8ff651a7012e3b
legetic <Legetic@hotmail.com>
2016-02-25 19:51:29 +0100

    asd

--- js/script1.js
+++ js/script1.js
@@ -1,3 +1,27 @@
+
+$(window).on("load", function() {
+    $('body').addClass('loaded');
+});
+
+
 $( document ).ready(function() {
-    
+  $(function() {
+      FastClick.attach(document.body);
+  });
+
+  if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+mobile = true;
+    $body.addClass('mobile');
+  }else{
+    mobile = false;
+    $body.addClass('notMobile');
+  }
+
+
+  if(Modernizr.mq('(max-width: 992px)')){
+  $body.addClass('small');
+  }else{
+    $body.removeClass('small');
+  }
+
 });
