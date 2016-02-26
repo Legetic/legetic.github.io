d842aace83156b59cdb86e665f80409f4e83c41b
Legetic <Legetic@hotmail.com>
2016-02-26 13:04:59 +0100

    hej

--- js/vendor/pace.js
+++ js/vendor/pace.js
@@ -294,7 +294,7 @@
       }
       if (!this.lastRenderedProgress || this.lastRenderedProgress | 0 !== this.progress | 0) {
         el.children[0].setAttribute('data-progress-text', "" + (this.progress | 0) + "%");
-        console.log(this.progress);
+//hej här la jag till
         $("#center-box").css("width",this.progress + "%");
         if (this.progress >= 100) {
           progressStr = '99';
