aed922ef80329f0adf2bdd27c2230e9759d80f5b
legetic <Legetic@hotmail.com>
2016-03-01 21:02:59 +0100

    navbutts

--- js/app.js
+++ js/app.js
@@ -19,28 +19,46 @@ $( document ).ready(function() {
     mobile = false;
   }
 
-/*if(!mobile){
+  /*if(!mobile){
   $("#box").mousemove(function(event){
-            var relX = event.pageX - $(this).offset().left;
-            var relY = event.pageY - $(this).offset().top;
-            var relBoxCoords = "(" + relX + "," + relY + ")";
-            $(".mouse-cords").text(relBoxCoords);
-        });
+  var relX = event.pageX - $(this).offset().left;
+  var relY = event.pageY - $(this).offset().top;
+  var relBoxCoords = "(" + relX + "," + relY + ")";
+  $(".mouse-cords").text(relBoxCoords);
+});
 }*/
+if(Modernizr.mq('(max-width: 992px)')){
+  $body.addClass('small');
+}else{
+  $body.removeClass('small');
+}
+
+$(window).resize(function() {
   if(Modernizr.mq('(max-width: 992px)')){
     $body.addClass('small');
   }else{
     $body.removeClass('small');
   }
-/*
-  swal({   title: "Error!",
-  text: "Here's my error message!",
-  type: "error",
-  confirmButtonText: "Cool" });
+});
 
 
-  $( "#center-box" ).on( "click", function() {
-    console.log( "rip" );
+  $(".navButton").on("click", function(){
+  /*  e.preventDefault(); KAN BEHÖVAS OM JAG BYTER TILL EN a*/
+  $(".navButton").removeClass('active'); // remove first
+  $(this).addClass('active');
+
   });
+/*
+swal({   title: "Error!",
+text: "Here's my error message!",
+type: "error",
+confirmButtonText: "Cool" });
+
+
+$( "#center-box" ).on( "click", function() {
+console.log( "rip" );
+});
 */
+
+
 });
