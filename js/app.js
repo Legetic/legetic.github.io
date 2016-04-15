4006f55b04c3191b97adfb8fed739868c1fb5897
Legetic <Legetic@hotmail.com>
2016-04-15 23:06:01 +0200

    hej

--- js/app.js
+++ js/app.js
@@ -73,7 +73,7 @@ $(window).resize(function() {
     $("#contact-button").on("click", function(){
     /*  e.preventDefault(); KAN BEHÖVAS OM JAG BYTER TILL EN a*/
       $( "#contact" ).toggleClass( "extended" )
-      $("#contact-button").toggleClass("fa-share");
+      $("#contact-button").toggleClass("fa-envelope");
       $("#contact-button").toggleClass("fa-times");
 
     });
