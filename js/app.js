e59c24618383011d8ce12271d58e2103d3553eb0
Legetic <Legetic@hotmail.com>
2016-04-21 13:50:21 +0200

    hej

--- js/app.js
+++ js/app.js
@@ -13,6 +13,15 @@ $( document ).ready(function() {
   var $contact =   $( "#contact" );
 
 
+  $(document).ready(function () {
+  $.cookieCuttr({
+  cookieAnalytics: false,
+  cookieMessage: 'We use cookies on this website, you can <a href="{{cookiePolicyLink}}" title="read about our cookies">read about them here</a>. To use the website as intended please...',
+  cookiePolicyLink: 'http://cookiecuttr.com/'
+  });
+  });
+
+
   $(function() {
     FastClick.attach(document.body);
   });
